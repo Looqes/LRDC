b952fc4542f9e46b5a20f990b90e4fc340ef1b0f
Looqes <lucas.belderink@student.uva.nl>
2023-05-23 17:22:44 +0200

    added greedy algorithm for picking clause matchings to come to a difference expression

--- comparator.py
+++ comparator.py
@@ -75,12 +75,43 @@ def partial_overlap_compare(expression1, expression2):
     return possible_differences
 
 
-# TODO: greedy function
+# Algorithm that picks overlap pairs between clauses with a greedy method
+# It iteratively checks each clause in the first expression and picks
+# a highest scoring clause it matches to from the second expression
 def greedy_difference_expression(possible_differences):
-    groups = dict()
-    for diff in possible_differences:
-        indexes = diff[0]
-        clause_difference = diff[1]
+    # Remainder clauses from the first expression with some partial overlap
+    left_clauses = {pair[0] for pair in possible_differences}
+
+    # Set to keep track of which clauses from the second expression have been
+    # picked, for clauses in an expression cannot be picked more than once
+    picked_clauses = set()
+
+    difference_expression = set()
+    for clause_nr in left_clauses:
+        matchings = [pair for pair in possible_differences if pair[0] == clause_nr]
+
+        # If only a single matching with the clause from the first expression
+        # exists, check if it fits and add it if it does.
+        if len(matchings) == 1:
+            if matchings[0][1] not in picked_clauses:
+                difference_expression.add(matchings[0])
+                picked_clauses.add(matchings[0][1])
+        # If there are multiple options for matchings, check if they can be
+        # added based on their score (highest score first)
+        else:
+            rated = [(matching, possible_differences[matching])
+                     for matching in matchings]
+            rated = sorted(rated, key = lambda x: x[1].score, reverse = True)
+
+            for matching in rated:
+                if matching[0][1] not in picked_clauses:
+                    difference_expression.add(matching[0])
+                    picked_clauses.add(matching[0][1])
+                    # If a matching has been picked, move on to the next
+                    # clause from the first expression
+                    break
+
+    return difference_expression
 
 
 # Function to find all unique subsets of a set of pairs of integers that do not
@@ -151,24 +182,27 @@ print(list(possible_clause_differences.keys()))
 # print(possible_clause_differences)
 
 
-
-print("\nStep 4, Find all possible difference expressions...")
-difference_expressions = find_all_difference_expressions(
-                         list(possible_clause_differences.keys()))
-[print(x) for x in difference_expressions]
+print("\nStep4, Greedy")
+result = greedy_difference_expression(possible_clause_differences)
 
 
-print("\nStep 5, weigh difference expressions based on the weight of their" +
-      " contained clause differences")
-weighed_difference_expressions = []
-for difference_expression in difference_expressions:
-    weighed_difference_expressions.append(
-        (difference_expression,
-        sum([possible_clause_differences[clause_difference].score for clause_difference in difference_expression]))
-    )
+# print("\nStep 4, Find all possible difference expressions...")
+# difference_expressions = find_all_difference_expressions(
+#                          list(possible_clause_differences.keys()))
+# [print(x) for x in difference_expressions]
 
-# Sorting to get best scoring expressions (most minimal = highest score)
-weighed_difference_expressions = sorted(weighed_difference_expressions, key=lambda pair: pair[1], reverse = True)
 
-[print(x) for x in weighed_difference_expressions]
+# print("\nStep 5, weigh difference expressions based on the weight of their" +
+#       " contained clause differences")
+# weighed_difference_expressions = []
+# for difference_expression in difference_expressions:
+#     weighed_difference_expressions.append(
+#         (difference_expression,
+#         sum([possible_clause_differences[clause_difference].score for clause_difference in difference_expression]))
+#     )
+
+# # Sorting to get best scoring expressions (most minimal = highest score)
+# weighed_difference_expressions = sorted(weighed_difference_expressions, key=lambda pair: pair[1], reverse = True)
+
+# [print(x) for x in weighed_difference_expressions]
 
