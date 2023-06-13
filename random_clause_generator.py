6ba692a95f0c1b8404b395fbc856e39b49adb004
Looqes <lucas.belderink@student.uva.nl>
2023-06-13 14:22:05 +0200

    random clause generator now takes args for its clause structure and output to either file in DIMACS-format or as cnfgen expression objects is now toggleable.

--- random_clause_generator.py
+++ random_clause_generator.py
@@ -1,7 +1,7 @@
 import cnfgen
 
-def generate_random_clauses():
-    print("Please specify parameters for random clause generation: ")
+def generate_random_clauses(k1, n1, m1, k2, n2, m2, use_dimacs):
+    # print("Please specify parameters for random clause generation: ")
     # k1 = input("Amount of literals per clause in first expression: ")
     # k1 = int(k1) if k1.isdigit() else {print("Not a number!"), exit(0)}
     # n1 = input("Amount of variables to choose from in first expression: ")
@@ -16,16 +16,22 @@ def generate_random_clauses():
     # m2 = input("Amount of clauses to generate for first expression: ")
     # m2 = int(m2) if m2.isdigit() else {print("Not a number!"), exit(0)}
 
-    k1, n1, m1 = 5, 20, 5
-    k2, n2, m2 = 5, 20, 5
+    # k1, n1, m1 = 5, 20, 5
+    # k2, n2, m2 = 5, 20, 5
 
     # k = amount of literals per clause, 
     # n = amount of variables, 
     # m = amount of clauses to generate
-    first_expr = cnfgen.RandomKCNF(k1, n1, m1).to_dimacs()
-    with open("cnffiles/first_expression", "w") as text_file:
-        text_file.write(first_expr)
+    first_expr = cnfgen.RandomKCNF(k1, n1, m1)
+    second_expr = cnfgen.RandomKCNF(k2, n2, m2)
 
-    second_expr = cnfgen.RandomKCNF(k2, n2, m2).to_dimacs()
-    with open("cnffiles/second_expression", "w") as text_file:
-        text_file.write(second_expr)
+
+    if use_dimacs:
+        with open("cnffiles/first_expression", "w") as text_file:
+            text_file.write(first_expr.to_dimacs())
+
+        with open("cnffiles/second_expression", "w") as text_file:
+            text_file.write(second_expr.to_dimacs())
+
+    else:
+        return first_expr, second_expr
